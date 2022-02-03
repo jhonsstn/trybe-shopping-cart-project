1264ae3e7cbdcf95b355443ee22657e701511777
Jhon Santana <jhonsstn@gmail.com>
2022-02-03 16:59:36 -0300

    finalizada função fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui.
+const fetchProducts = async (product) => {
+  if (!product) {
+    const error = new Error('You must provide an url');
+    return error;
+  }
+
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,7 +2,35 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('1 - The function fetchProducts', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should be a function.', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('should call "fetch()" if called with "computador" argument.', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('should call "fetch()" with the "API_URL" endpoint if called with "computador" argument.', () => {
+    const API_URL =
+      'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+    fetchProducts('computador');
+
+    expect(fetch).toHaveBeenCalledWith(API_URL);
+  });
+
+  it('should return the "computadorSearch" data structure if called with "computador" argument.', async () => {
+    const resultObject = await fetchProducts('computador');
+    expect(resultObject).toMatchObject(computadorSearch);
+  });
+
+  it('should return the "You must provide an url" error if called with no arguments', async () => {
+    const noArguments = await fetchProducts();
+    expect(noArguments).toEqual(new Error('You must provide an url'));
+  });
 });
