f33f8f7902657e90e9db05bbe7635f8a89795acc
Jhon Santana <jhonsstn@gmail.com>
2022-02-04 15:06:26 -0300

    finalizada função getSavedCartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -3,7 +3,18 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 
 localStorageSimulator('getItem');
 
-describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('4 - The getSavedCartItems function', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should call the "localStorage.getItem" method when executed.', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+
+  it('should call the "localStorage.setItem" method with "cartItems" and  "<ol><li>Item</li></ol>" arguments if called with "<ol><li>Item</li></ol>" argument.', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,12 +7,12 @@ describe('4 - The "saveCartItems" functions', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  it('should call the "localStorage.setItem" method if called with "<ol><li>Item</li></ol>" argument.', async () => {
+  it('should call the "localStorage.setItem" method if called with "<ol><li>Item</li></ol>" argument.', () => {
     saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalled();
   });
 
-  it('should call the "localStorage.setItem" method with "cartItems" and  "<ol><li>Item</li></ol>" arguments if called with "<ol><li>Item</li></ol>" argument.', async () => {
+  it('should call the "localStorage.setItem" method with "cartItems" and  "<ol><li>Item</li></ol>" arguments if called with "<ol><li>Item</li></ol>" argument.', () => {
     const item = '<ol><li>Item</li></ol>';
     saveCartItems(item);
     expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', item);
