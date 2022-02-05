6b8730c1755b4c51ad105969d2cf6fa39e59e1c4
Jhon Santana <jhonsstn@gmail.com>
2022-02-05 15:37:34 -0300

    Requisito 5 e 6 completo

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const cartContainer = document.querySelector('.cart__items');
+const totalPriceElement = document.querySelector('.total-price');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -31,9 +34,23 @@ function getSkuFromProductItem(event) {
   return itemSku;
 }
 
+let totalPrice = 0;
+const addToPrice = (itemPrice) => {
+  totalPrice += itemPrice;
+  totalPriceElement.innerText = totalPrice;
+};
+
+const removeFromPrice = (element) => {
+  const elementText = element.innerText;
+  const itemPrice = parseFloat(elementText.split('$')[1]);
+  totalPrice -= itemPrice;
+  totalPriceElement.innerText = totalPrice;
+};
+
 function cartItemClickListener(event) {
   const element = event.target;
   element.remove();
+  removeFromPrice(event.target);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -53,15 +70,13 @@ const saveLocalStorage = (sku, name, salePrice) => {
 };
 
 const addToCart = async (event) => {
-  const cartContainer = document.querySelector('.cart__items');
   const itemSku = getSkuFromProductItem(event);
   const item = await fetchItem(itemSku);
   const { id: sku, title: name, price: salePrice } = item;
+  addToPrice(salePrice);
   const itemOnCart = createCartItemElement({ sku, name, salePrice });
-
   saveLocalStorage(sku, name, salePrice);
   cartContainer.appendChild(itemOnCart);
-  console.log(cartContainer);
 };
 
 const addEventListenerToButtons = () => {
@@ -84,9 +99,7 @@ const productsListing = async () => {
 
 const loadLocalStorage = () => {
   if (getSavedCartItems()) {
-    const cartContainer = document.querySelector('.cart__items');
     const productsToLoad = JSON.parse(getSavedCartItems());
-    console.log(productsToLoad);
     productsToLoad.forEach((product) => {
       const { sku, name, salePrice } = product;
       const itemOnCart = createCartItemElement({ sku, name, salePrice });
@@ -95,7 +108,16 @@ const loadLocalStorage = () => {
   }
 };
 
+const clearCart = () => {
+  const clearCartButton = document.querySelector('.empty-cart');
+  clearCartButton.addEventListener('click', () => {
+    cartContainer.innerHTML = null;
+    localStorage.clear();
+  });
+};
+
 window.onload = () => {
   productsListing();
   loadLocalStorage();
+  clearCart();
 };
